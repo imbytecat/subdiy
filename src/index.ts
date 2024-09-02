c9f09cc053bfaa4b1d6ef969473ca98153fccef3
imbytecat <imbytecat@gmail.com>
2024-09-02 14:08:19 +0800

    chore: add logger and CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { authRoutes } from '@server/routes/auth'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
 
-app.route('/', authRoutes)
+app.use('*', logger())
+  .use('/api/*', cors())
+  .route('/api', authRoutes)
 
 export default app
