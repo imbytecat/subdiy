42b07831eafe37a84a668f415ec8b7e7ad74bce9
imbytecat <imbytecat@gmail.com>
2024-09-02 15:46:48 +0800

    feat(auth): add login method to auth route

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -4,6 +4,8 @@ import { db } from '@db/client'
 import { z } from 'zod'
 import { user as userTable } from '@db/schema'
 import { errorResponse, successResponse } from '@server/utils/responseUtil'
+import { eq } from 'drizzle-orm'
+import { HTTPException } from 'hono/http-exception'
 
 const UsernamePasswordSchema = z.object({
   username: z.string(),
@@ -13,6 +15,7 @@ const UsernamePasswordSchema = z.object({
 export const authRoutes = new Hono()
   .basePath('/auth')
   .post('/register', zValidator('json', UsernamePasswordSchema), async (c) => {
+    // TODO: 把全局错误处理写成中间件，处理包括验证错误在内的所有异常
     const { username, password } = c.req.valid('json')
     try {
       await db.insert(userTable).values({
@@ -21,7 +24,22 @@ export const authRoutes = new Hono()
         role: 'user',
       })
     } catch (error) {
-      return c.json(errorResponse('错误'))
+      return c.json(errorResponse('数据库错误', 500))
     }
-    return c.json(successResponse('成功'))
+
+    return c.json(successResponse('注册成功'))
+  })
+  .post('/login', zValidator('json', UsernamePasswordSchema), async (c) => {
+    const { username, password } = c.req.valid('json')
+    const user = await db
+      .select()
+      .from(userTable)
+      .where(eq(userTable.username, username))
+
+    if (!user.length || user[0].password !== password) {
+      c.status(401)
+      return c.json(errorResponse('用户名或密码不正确', 401))
+    }
+
+    return c.json(successResponse(null, `Hello, ${username}`))
   })
